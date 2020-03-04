7db5bd120938300affcc36a6e55dc400fc3251a2
cxp <853113943@qq.com>
2020-03-04 17:48:05 +0800

    代码文件

--- app/src/main/java/com/example/kongheapp/activity/SetActivity.java
+++ app/src/main/java/com/example/kongheapp/activity/SetActivity.java
@@ -10,6 +10,5 @@ public class SetActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.set_layout);
-
     }
 }
