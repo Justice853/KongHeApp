4a8bd3b966284924106b6ee41bad5b25264f68ea
cxp <853113943@qq.com>
2020-03-11 21:12:08 +0800

    最新更新

---
+++ app/src/main/java/com/example/kongheapp/activity/QuanBuActivity.java
@@ -0,0 +1,22 @@
+package com.example.kongheapp.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class QuanBuActivity extends Activity {
+    TextView title;
+    ImageView fanhui;
+    GridView application;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
